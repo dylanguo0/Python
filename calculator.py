0f5e11cc23054bdc9e573e76a3e0b61b5abe261a
dylanguo0 <dylanguo@riccarton.school.nz>
2025-03-12 14:18:40 +1300

    Added comments

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+''' This program asks the user for 2 numbers and an operator then calculates the answer '''
+
+# Function for calculator
 def calculate(n1, operator, n2):
     if operator == '+':
         return n1 + n2
@@ -10,6 +13,7 @@ def calculate(n1, operator, n2):
     elif operator == '^':
         return n1 ** n2
 
+# Asks the user for the 2 numbers and check it's a number
 while True:
     try:
         n1 = float(input('Enter a number: '))
@@ -24,10 +28,15 @@ while True:
     except ValueError:
         print('Please enter a number')
 
-operator = 0
-while operator != '+' and operator != '-' and operator != '*' and operator != '/' and operator != '^':
+# Asks the user for an operator and check it's valid
+while True:
     operator = input('Enter an operator (+, -, *, /, ^): ')
+    if operator != '+' and operator != '-' and operator != '*' and operator != '/' and operator != '^':
+        print('Please enter a valid operator')
+    else:
+        break
 
+# Calculates it while checking for any errors
 try:
     print(calculate(n1, operator, n2))
 except OverflowError:
