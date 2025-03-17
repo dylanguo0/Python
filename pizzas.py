2df00a0cfde2dab9450dedfa2d3b10f368053f1e
dylanguo0 <dylanguo@riccarton.school.nz>
2025-03-18 09:27:37 +1300

    Added a try except to test for invalid inputs and fixed some minor things

--- pizzas.py
+++ pizzas.py
@@ -6,9 +6,18 @@ number = []
 
 # Goes through all the different types of pizzas and asks the user how many of each they want
 for item in pizzas:
-    number.append(int(input(f'How many {item} pizzas do we want? ')))
+    while True:
+        try:
+            amount = int(input(f'How many {item} pizzas do we want? '))
+            if amount < 0:
+                print('Please enter a valid amount')
+            else:
+                number.append(amount)
+                break
+        except ValueError:
+            print('Please enter a valid amount')
 
 # Goes through all the pizzas and print how many the user ordered unless it's 0
 for i in range(len(pizzas)):
-    if number[i - 1] != 0:
-        print(f'{pizzas[i - 1].title()}: {number[i - 1]}')
+    if number[i] != 0:
+        print(f'{pizzas[i].title()}: {number[i]}')
