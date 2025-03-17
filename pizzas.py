7d47ede121cac0318c7565f2fc4eef7cdf9820b9
dylanguo0 <dylanguo@riccarton.school.nz>
2025-03-18 09:22:33 +1300

    Added some comments and if the number of pizzas equals 0 skip it

--- pizzas.py
+++ pizzas.py
@@ -4,8 +4,11 @@
 pizzas = ['cheese', 'chicken', 'pepperoni', 'veggie']
 number = []
 
+# Goes through all the different types of pizzas and asks the user how many of each they want
 for item in pizzas:
     number.append(int(input(f'How many {item} pizzas do we want? ')))
 
+# Goes through all the pizzas and print how many the user ordered unless it's 0
 for i in range(len(pizzas)):
-    print(f'{pizzas[i - 1].title()}: {number[i - 1]}')
+    if number[i - 1] != 0:
+        print(f'{pizzas[i - 1].title()}: {number[i - 1]}')
