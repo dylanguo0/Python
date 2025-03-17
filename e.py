6c350a15d9f0df11659237a3ecb29474d42bdef9
dylanguo0 <dylanguo@riccarton.school.nz>
2025-03-18 09:14:44 +1300

    Started the program and defined the lists

---
+++ e.py
@@ -0,0 +1,5 @@
+e = (1 + 1 / 1000000) ** 1000000
+phi = (1 + 5 ** 0.5) / 2
+
+print(f'e: {e}')
+print(f'Golden ratio: {phi}')
---
+++ pizzas.py
@@ -0,0 +1,5 @@
+''' This program asks the user how many cheese, chicken, pepperoni, and veggie pizzas they want '''
+
+# Defines the list for all the pizzas and number of them
+pizzas = ['cheese', 'chicken', 'pepperoni', 'veggie']
+number = []
