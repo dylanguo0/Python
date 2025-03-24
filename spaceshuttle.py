5ca5fe06811e75364fd969d61a786b30c17551ae
dylanguo0 <dylanguo@riccarton.school.nz>
2025-03-25 09:01:22 +1300

    After asking the user for speeds, print all the unsafe speeds

--- spaceshuttle.py
+++ spaceshuttle.py
@@ -1,7 +1,7 @@
 ''' This program asks the user for a descent speed of a space shuttle until they type in 'conclude'. If the user user enters an invalid input, the program will
     print 'Error, invalid input', and ask again. After the user types in conclude, it'll print out all the unsafe speeds, which are speeds faster than 10m/s'''
 
-# Defines the unsafe speed
+# Defines the unsafe speed and break loop condition
 UNSAFE = 10
 BREAK_LOOP = 'conclude'
 
@@ -15,5 +15,12 @@ while True:
         break
     else:
         speed = float(speed)
+    
+    # Adds it to the list of unsafe speeds if it is unsafe
     if speed > UNSAFE:
         unsafe_speeds.append(speed)
+
+print(f'There were {len(unsafe_speeds)} space shuttles faster than the safe speed.')
+print('The unsafe speeds are')
+for speed in unsafe_speeds:
+    print(speed)
