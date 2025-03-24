3e645f116c33b6cc83f720849f4e805a13a3a62e
dylanguo0 <dylanguo@riccarton.school.nz>
2025-03-25 08:52:47 +1300

    Started the program and defined the variables which will be used in the program

---
+++ spaceshuttle.py
@@ -0,0 +1,8 @@
+''' This program asks the user for a descent speed of a space shuttle until they type in 'conclude'. If the user user enters an invalid input, the program will
+    print 'Error, invalid input', and ask again. After the user types in conclude, it'll print out all the unsafe speeds, which are speeds faster than 10m/s'''
+
+# Defines the unsafe speed
+UNSAFE = 10
+
+# Defines the list which contains all the unsafe speeds
+unsafe_speeds = []
